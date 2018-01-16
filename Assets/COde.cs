4971db083fa4e7fa1e2ad7d3bc93ec5a35ce2284
Rachelle Anne De Villa <Rachelle Anne@ASUS_X550.mit.edu>
2018-01-16 21:29:56 +0800

    init

---
+++ Assets/COde.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COde : MonoBehaviour {
+	Rigidbody _rb;
+	// Use this for initialization
+	void Start () {
+		_rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput ();
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.name == "Cube(5)") {
+			col.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		}
+	}
+
+	public void ProcessInput ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			_rb.AddRelativeForce (Vector3.up*10);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Vector3.left);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.right);
+		}
+	}
+}
