3ffcb767500be1d00d3f66238b8a3149a492ab60
Rachelle Anne De Villa <Rachelle Anne@ASUS_X550.mit.edu>
2018-01-23 15:09:33 +0800

    M3T11

--- Assets/Oncollision.cs
+++ Assets/Oncollision.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Oncollision : MonoBehaviour {
 
-	// Use this for initialization
+	GameObject r1;
+	public float landing_pad = 20f;
+
 	void Start () {
-		
+		r1 = GameObject.Find ("Rocket_1");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		trans ();
 	}
 
-	public void c (){
-		
+
+	public void trans (){
+		float x = Vector3.Distance (r1.transform.position, transform.position);
+		//print (x);
+		if (x < landing_pad) {
+			GetComponent<Renderer> ().material.color = Color.yellow;
+		} else {
+			GetComponent<Renderer> ().material.color = Color.red;
+		}
 	}
 }
--- Assets/scene2/yey.cs
+++ Assets/scene2/yey.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,8 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.Rotate(Vector3.right * Time.deltaTime * 15);
+
+
+    }
 }
